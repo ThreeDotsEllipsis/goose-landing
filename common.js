b63f0099f4c3fd77249eeb891fe38089dc085424
threedots <threedotsellipsis@protonmail.com>
2023-02-15 21:19:26 +0500

    Remove repeats of header and footer on each page

--- common.js
+++ common.js
@@ -1,3 +1,55 @@
+// ----- Create header and footer --------------------------------
+body = document.querySelector('body');
+body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <div class="header">
+        <div class="nav-bar">
+            <ul class="nav-bar-list">
+                <li> <a href="order.html">Order</a> </li>
+                <li> <a href="gallery.html">Gallery</a> </li>
+                <li> <a href="about.html">Contacts</a> </li>
+            </ul>
+        </div>
+        <div class="logo"><a href="index.html">GOOSE</a></div>
+        <button type="button" class="nav-button">≣</button>
+    </div>
+    `
+);
+
+body.insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class="footer">
+        <div class="logo">GOOSE</div>
+        <div class="links">
+            <div class="tel-num">89999999999</div>
+
+            <div class="address">
+                <ul class="address-list">
+                    <li>Headquater: Las Vegas</li>
+                    <li>Shipment: Glubinka</li>
+                </ul>
+            </div>
+
+            <div class="socials">
+                <ul class="socials-list">
+                    <li>Instagram: biggoose</li>
+                    <li>Facebook: biggoose</li>
+                </ul>
+            </div>
+
+            <div class="socials">
+                <ul class="socials-list">
+                    <li>VK: biggoose</li>
+                    <li>Email: biggoose@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+    `
+);
+// --------------------------------------------------------------
+
 let navButton = document.querySelector(".nav-button");
 let navBar = document.querySelector(".nav-bar");
 
