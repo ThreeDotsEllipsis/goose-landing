619788cdbf3e5a80bbf6b4a25f1e98c66e14a20c
threedots <threedotsellipsis@protonmail.com>
2023-02-21 12:07:57 +0500

    Remake order page image scrolling

--- js/order.js
+++ js/order.js
@@ -1,56 +1,9 @@
-
-let sizeButtons = document.querySelectorAll(".size-buttons > button");
-let chosenSizeButton = document.querySelector(".size-buttons > .chosen");
-
-let colorButtons = document.querySelectorAll(".color-buttons > button");
-let chosenColorButton = document.querySelector(".color-buttons > .chosen");
-
-let gooseImages = document.querySelectorAll(".image img");
-let chosenImage = document.querySelector(".image .chosen");
-
-sizeButtons.forEach((size) => {
-    size.addEventListener("click", () => {
-        if (chosenSizeButton != size) {
-            chosenSizeButton.classList.remove("chosen");
-            size.classList.add("chosen");
-            chosenSizeButton = size;
-        }
-    });
-});
+let colorButtons = document.querySelectorAll(".color-buttons > input");
+let gooseArray = document.querySelector(".image-array");
 
 colorButtons.forEach((color) => {
     color.addEventListener("click", () => {
-        if (chosenColorButton != color) {
-            chosenColorButton.classList.remove("chosen");
-            color.classList.add("chosen");
-            chosenColorButton = color;
-
-            chosenImage.classList.remove("chosen");
-
-            let chose = document.querySelector(`.image .${color.textContent}`);
-            chose.style.right = "0px";
-
-            let element = chose;
-            let count = 1;
-
-            while (element.nextElementSibling) {
-                element = element.nextElementSibling;
-                element.style.right = `-${400 * count}px`;
-                count += 1;
-            }
-
-            element = chose;
-            count = 1;
-
-            while (element.previousElementSibling) {
-                element = element.previousElementSibling;
-                element.style.right = `${400 * count}px`;
-                count += 1;
-            }
-
-            chose.classList.add("chosen");
-            chosenImage = chose;
-        }
+        gooseArray.style.right = `${400 * color.getAttribute("data-id")}px`;
     });
 });
 
