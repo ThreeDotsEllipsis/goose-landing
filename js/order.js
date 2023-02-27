fd3c2435cae576e9d9246eb08920dfb21b260e5a
threedots <threedotsellipsis@protonmail.com>
2023-02-27 20:10:48 +0500

    Order page color selection with multiple images

--- js/order.js
+++ js/order.js
@@ -1,9 +1,41 @@
+let scrollRightButton = document.getElementById("scroll-right");
+let scrollLeftButton = document.getElementById("scroll-left");
+
 let colorButtons = document.querySelectorAll(".color-buttons > input");
 let gooseArray = document.querySelector(".image-array");
+let gooseImages = document.querySelectorAll(".image-array img")
 
 colorButtons.forEach((color) => {
     color.addEventListener("click", () => {
-        gooseArray.style.right = `${400 * color.getAttribute("data-id")}px`;
+        gooseImages.forEach((el) => {
+            el.style.display = "none";
+        });
+
+        let imgs = document.querySelectorAll(`[src*="${color.id}"]`)
+        imgs.forEach((el) => {
+            el.style.display = "block";
+        });
     });
 });
 
+gooseImages.forEach((el, index) => {
+    if (index % 2 == 0)
+        el.style.right = `-400px`;
+});
+
+
+let index = 0;
+
+scrollRightButton.addEventListener("click", () => {
+    if (index < 1) {
+        index += 1;
+        gooseArray.style.right = `${400 * index}px`;
+    }
+});
+
+scrollLeftButton.addEventListener("click", () => {
+    if (index > 0) {
+        index -= 1;
+        gooseArray.style.right = `${400 * index}px`;
+    }
+});
