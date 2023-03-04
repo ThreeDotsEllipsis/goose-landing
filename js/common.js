5893eca8c02c742041d6a214a58506d729589b31
threedots <threedotsellipsis@protonmail.com>
2023-03-04 19:03:23 +0500

    Add more pages

--- js/common.js
+++ js/common.js
@@ -1,3 +1,51 @@
+body = document.querySelector('body');
+body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <header>
+        <div class="logo"><a href="index.html">Гусь</a></div>
+        <input type="checkbox" id="nav-button">
+        <label for="nav-button">≣</label>
+        <nav>
+            <ul class="nav-content">
+                <li><a href="order.html">Заказать</a></li>
+                <li><a href="gallery.html">Галлерея</a></li>
+                <li><a href="about.html">Контакты</a></li>
+            </ul>
+        </nav>
+    </header>
+    `
+);
+
+body.insertAdjacentHTML(
+    'beforeend',
+    `
+        <div class="footer">
+        <div class="logo">GOOSE</div>
+        <div class="links">
+            <div class="tel-num">89999999999</div>
+            <div class="address">
+                <ul class="address-list">
+                    <li>Headquater: Las Vegas</li>
+                    <li>Shipment: Glubinka</li>
+                </ul>
+            </div>
+            <div class="socials">
+                <ul class="socials-list">
+                    <li>Instagram: biggoose</li>
+                    <li>Facebook: biggoose</li>
+                </ul>
+            </div>
+            <div class="socials">
+                <ul class="socials-list">
+                    <li>VK: biggoose</li>
+                    <li>Email: biggoose@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+`
+);
+
 let contentElements = document.querySelectorAll(".main-content > div:not(.delimiter)");
 
 function checkVisible(elm) {
