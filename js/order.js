0197bb4840e5da7421382930998108307f6ac902
threedots <threedotsellipsis@protonmail.com>
2023-10-12 21:33:04 +0500

    Add ability to click on the image index radio button to focus it into view

--- js/order.js
+++ js/order.js
@@ -1,7 +1,7 @@
 let images = {
-    "black": ["imgs/black.avif", "imgs/black2.jpg"],
-    "white": ["imgs/white.avif", "imgs/white2.jpg"],
-    "brown": ["imgs/brown.avif", "imgs/brown2.jpg"],
+    black: ["imgs/black.avif", "imgs/black2.jpg"],
+    white: ["imgs/white.avif", "imgs/white2.jpg"],
+    brown: ["imgs/brown.avif", "imgs/brown2.jpg"],
 };
 
 let imageLine = document.querySelector(".image-line");
@@ -24,6 +24,7 @@ function createImages(type) {
     let len = images[type].length;
 
     for (i = 0; i < len; i++) {
+        let trigger = document.createElement("div");
         let radio = document.createElement("input");
         radio.type = "radio";
         radio.name = "index";
@@ -32,8 +33,15 @@ function createImages(type) {
             radio.checked = true;
         }
         radio.id = i;
-        imageIndex.appendChild(radio);
+        trigger.appendChild(radio);
 
+        trigger.addEventListener("click", () => {
+            let shiftAmount = document.querySelector(".image-view").offsetWidth;
+            imageLine.style.left = `-${shiftAmount * radio.id}px`;
+            radio.checked = true;
+        });
+
+        imageIndex.appendChild(trigger);
 
         let image = document.createElement("img");
         image.src = images[type][i];
@@ -55,8 +63,7 @@ function switchImages(button) {
         if (index > 0) {
             index -= 1;
         }
-    }
-    else {
+    } else {
         if (index < imageLine.childNodes.length - 1) {
             index += 1;
         }
@@ -72,8 +79,8 @@ function animateUnderline(target, underline) {
     let rect = colorLabel.getBoundingClientRect();
 
     underline.style.top = `${rect.top + rect.height / 1.2}px`;
-    underline.style.left = `${rect.left + rect.width / 100 * 10}px`;
-    underline.style.width = `${rect.width - rect.width / 100 * 20}px`;
+    underline.style.left = `${rect.left + (rect.width / 100) * 10}px`;
+    underline.style.width = `${rect.width - (rect.width / 100) * 20}px`;
 }
 
 colorInputs.forEach((input) => {
@@ -107,8 +114,14 @@ nextButton.addEventListener("click", switchImages);
 createImages("white");
 
 window.addEventListener("resize", () => {
-    animateUnderline(document.querySelector(".size input:checked"), sizeUnderline);
-    animateUnderline(document.querySelector(".color input:checked"), colorUnderline);
+    animateUnderline(
+        document.querySelector(".size input:checked"),
+        sizeUnderline
+    );
+    animateUnderline(
+        document.querySelector(".color input:checked"),
+        colorUnderline
+    );
 
     let shiftAmount = document.querySelector(".image-view").offsetWidth;
     let index = Number(document.querySelector(".index input:checked").id);
